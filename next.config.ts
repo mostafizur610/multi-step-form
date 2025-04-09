3abd58d0827478d4c91261c4c83b684496ad430d
mostafizur610 <mostafizr.cse@gmail.com>
2025-04-10 00:26:48 +0600

    project setup with required dependency

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['example.com'], // Add domains for external images
+  },
+  // Other configurations...
+}
+
+module.exports = nextConfig
